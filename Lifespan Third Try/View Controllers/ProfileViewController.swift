6b5c4ebbf8e888799cc98ad2ba41e868f57c4793
John Pavley <jpavley@gmail.com>
2018-04-17 12:37:06 -0400

    wired up the profile controls to profile view controller

--- Lifespan Third Try/View Controllers/ProfileViewController.swift
+++ Lifespan Third Try/View Controllers/ProfileViewController.swift
@@ -9,7 +9,46 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    // Personal Details
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var objectField: UITextField!
+    @IBOutlet weak var subjectField: UITextField!
+    @IBOutlet weak var possessiveField: UITextField!
+    
+    // Birth and Life Expectancy
+    
+    @IBOutlet weak var birthField: UITextField!
+    @IBOutlet weak var dayField: UITextField!
+    @IBOutlet weak var monthField: UITextField!
+    @IBOutlet weak var yearField: UITextField!
+    @IBOutlet weak var lifeExpenctancyField: UITextField!
 
+    @IBOutlet weak var daySlider: UISlider!
+    @IBOutlet weak var monthSlider: UISlider!
+    @IBOutlet weak var yearSlider: UISlider!
+    @IBOutlet weak var lifeExpencetancySlider: UISlider!
+    
+    // Life Factors
+
+    @IBOutlet weak var lifeFactorsField: UITextField!
+    @IBOutlet weak var activityField: UITextField!
+    @IBOutlet weak var stressField: UITextField!
+    @IBOutlet weak var riskField: UITextField!
+    @IBOutlet weak var geneticsField: UITextField!
+    
+    @IBOutlet weak var activitySlider: UISlider!
+    @IBOutlet weak var stressSlider: UISlider!
+    @IBOutlet weak var riskSlider: UISlider!
+    @IBOutlet weak var geneticsSlider: UISlider!
+
+
+
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
