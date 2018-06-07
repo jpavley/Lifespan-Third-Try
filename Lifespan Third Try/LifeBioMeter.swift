8f36e7ffb9b70e24080eaeed0650b0546154b0b5
John Pavley <jpavley@gmail.com>
2018-06-07 09:01:11 -0700

    tested LifeFactor, LifeBonus, testChronAgeHandAngle, tesBioAgeHandAngle

--- Lifespan Third Try/LifeBioMeter.swift
+++ Lifespan Third Try/LifeBioMeter.swift
@@ -9,9 +9,17 @@
 import Foundation
 import CoreGraphics
 
-/// Model of a partial dial that displays the difference between
-/// chronological age (what the calendar says) and biological age
-/// (what your doctor says).
+/// Model of a partial dial that displays the difference between chronological age (what the calendar says) and biological age (what your doctor says).
+///
+/// LifeFactor is the ratio of modified ALE to unmodified ALE (ale/mALE). LifeFactor is used to calculate biological age based on mALE.
+/// - If a user's mALE is higher than his ale then his LifeFactor is lower and his biological age is less than his chronological age.
+/// - If a users mALE is lower than his ale than his LifeFactor is higher and his biological age is more than his chrological age.
+///
+/// LifeBonus is a percentage that describes the positive or negative effect of LifeFactor on a user's life span.
+/// - If there is no effect (ale == mALE) LifeBonus is 0%
+/// - If there is a positivie effect LifeBonus is  > 0%
+/// - If there is a negative effect LifeBonus is < 0%
+/// - A negative LifeBonus is a "LifePenality"
 class LifeBioMeter {
     
     var chronologicalAge: CGFloat
@@ -28,10 +36,15 @@ class LifeBioMeter {
         self.chronologicalAge = chronologicalAge
         lifeFactor = ale/mAle
         biologicalAge = chronologicalAge * lifeFactor
-                
+        
         biologicalAge = biologicalAge > maxAge ? maxAge : biologicalAge
     }
     
+    /// Maps chronological age to a the angle of of the chronological hand of the biometer.
+    ///
+    /// - Age 120 (max age) = 120 degrees (4 o'clock)
+    /// - Age 60 = 0 degrees (12 o'clock)
+    /// - Age 0 = -120 degrees (8 o'clock)
     var chronAgeHandAngle: CGFloat {
         get {
             let percentOfMaxAge = chronologicalAge/maxAge
@@ -41,6 +54,7 @@ class LifeBioMeter {
     
     var bioAgeHandAngle: CGFloat {
         get {
+            // TODO: this is almost the same code as chronAgeHandAngle so it should be refactored into a common function
             let percentOfMaxAge = biologicalAge/maxAge
             return (percentOfMaxAge * totalDegrees) - startingAngle
         }
@@ -55,7 +69,9 @@ class LifeBioMeter {
     }
     var lifeBonus: Int {
         get {
-            return Int((1.0 - lifeFactor) * 100)
+            let r = 1.0 - lifeFactor
+            let p = r * 100
+            return Int(p)
         }
         
     }
--- Lifespan Third TryTests/LifespanBiometerModelTests.swift
+++ Lifespan Third TryTests/LifespanBiometerModelTests.swift
@@ -21,26 +21,87 @@ class LifespanBiometerModelTests: XCTestCase {
     }
     
     func testCronologicalAge() {
+        
         let lbmUT = LifeBioMeter(chronologicalAge: 100, ale: 100, mAle: 100)
         XCTAssertEqual(lbmUT.chronologicalAge, 100)
     }
     
     func testBiologicalAge() {
-        let lbm1UT = LifeBioMeter(chronologicalAge: 100, ale: 100, mAle: 100)
         
+        let lbm1UT = LifeBioMeter(chronologicalAge: 100, ale: 100, mAle: 100)
         XCTAssertEqual(lbm1UT.biologicalAge, 100)
         
         let lbm2UT = LifeBioMeter(chronologicalAge: 100, ale: 100, mAle: 120)
-        
         XCTAssertEqual(lbm2UT.biologicalAge.rounded(.down), 83.0)
         
         let lbm3UT = LifeBioMeter(chronologicalAge: 100, ale: 100, mAle: 80)
-        
         XCTAssertEqual(lbm3UT.biologicalAge.rounded(.down), 120)
-
+    }
+    
+    func testLifeFactor() {
         
+        let lbm1UT = LifeBioMeter(chronologicalAge: 100, ale: 100, mAle: 100)
+        XCTAssertEqual(lbm1UT.lifeFactor, 1)
+        
+        let lbm2UT = LifeBioMeter(chronologicalAge: 100, ale: 100, mAle: 120)
+        XCTAssertEqual(lbm2UT.lifeFactor, 100/120)
+        
+        let lbm3UT = LifeBioMeter(chronologicalAge: 100, ale: 100, mAle: 80)
+        XCTAssertEqual(lbm3UT.lifeFactor, 100/80)
+    }
+    
+    func testLifeBonus() {
+        
+        let lbm1UT = LifeBioMeter(chronologicalAge: 100, ale: 100, mAle: 100)
+        XCTAssertEqual(lbm1UT.lifeBonus, 0)
+        
+        let lbm2UT = LifeBioMeter(chronologicalAge: 100, ale: 100, mAle: 120)
+        XCTAssertEqual(lbm2UT.lifeBonus, 16)
+        
+        let lbm3UT = LifeBioMeter(chronologicalAge: 100, ale: 100, mAle: 80)
+        XCTAssertEqual(lbm3UT.lifeBonus, -25)
+    }
+    
+    func testChronAgeHandAngle() {
+        
+        let lbm1UT = LifeBioMeter(chronologicalAge: 120, ale: 100, mAle: 100)
+        XCTAssertEqual(lbm1UT.chronAgeHandAngle, 120)
+        
+        let lbm2UT = LifeBioMeter(chronologicalAge: 60, ale: 100, mAle: 100)
+        XCTAssertEqual(lbm2UT.chronAgeHandAngle, 0)
+        
+        let lbm3UT = LifeBioMeter(chronologicalAge: 0, ale: 100, mAle: 100)
+        XCTAssertEqual(lbm3UT.chronAgeHandAngle, -120)
+
+    }
+    
+    func tesBioAgeHandAngle() {
+        
+        let lbm1UT = LifeBioMeter(chronologicalAge: 120, ale: 120, mAle: 120)
+        XCTAssertEqual(lbm1UT.bioAgeHandAngle, 120)
+        
+        let lbm2UT = LifeBioMeter(chronologicalAge: 60, ale: 60, mAle: 60)
+        XCTAssertEqual(lbm2UT.bioAgeHandAngle, 0)
+        
+        let lbm3UT = LifeBioMeter(chronologicalAge: 0, ale: 0, mAle: 0)
+        XCTAssertEqual(lbm3UT.bioAgeHandAngle, -120)
         
     }
+
     
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
